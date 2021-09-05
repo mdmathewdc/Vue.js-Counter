14d43c596cd08f57d7e689fb9b83058402641f05
mdmathewdc <mathewdony007@gmail.com>
2021-09-05 19:48:01 +1000

    Added a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Vue.js Counter App"/>
-  <Counter msg="Counter app message"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="topnav">
 
-    <p>
     <!-- use the router-link component for navigation. -->
     <!-- specify the link by passing the `to` prop. -->
     <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About App</router-link>
+  </div>
 
-      <router-view />
+    <router-view />
 
-  </p>
+
+  <HelloWorld msg="Vue.js Counter App"/>
+  <Counter msg="Counter app message"/>
 
 </template>
 
@@ -30,12 +31,45 @@ export default {
 </script>
 
 <style>
+
+body {
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
 </style>
